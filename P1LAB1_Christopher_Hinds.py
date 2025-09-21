7434eddc293565787d9cd38b8e91397c286e4baa
ChrisHinds2202 <hindsc2202@student.faytechcc.edu>
2025-09-21 14:07:42 -0400

    Add files via upload

---
+++ P1LAB1_Christopher_Hinds.py
@@ -0,0 +1,10 @@
+ # Christopher Hinds
+ # 20 Sep 2025
+ # P1LAB1
+ # receeive input of first and last name and print out welcome message
+
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+
+print("Hello," ,firstName, lastName + "! Welcome to CTI-110")
